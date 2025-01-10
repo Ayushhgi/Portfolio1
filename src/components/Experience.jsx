a3dbd80a8206059b1a83e76ffa071b8f1f0c800d
Ayushhgi <goyal.ayush2424@gmail.com>
2025-01-10 21:14:03 +0530

    redo the changes of experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,11 @@
-// import React from 'react'
+import React from 'react'
 import html from '../assets/techs/html.jpg'
 import css from '../assets/techs/css.jpg'
 import javascript from '../assets/techs/javascript.png'
 import reactjs from '../assets/techs/react.png'
 import mongodb from '../assets/techs/mongodb.png'
 import nodejs from '../assets/techs/nodejs.png'
-import tailwind from '../assets/techs/tailwind.png'
+import tailwind from '../assets/techs/tailwind.png' // Corrected file name
 import github from '../assets/techs/github.png'
 
 const Experience = () => {
@@ -25,15 +25,16 @@ const Experience = () => {
       name='experience'
       className='bg-gradient-to-b from-gray-800 to-black w-full h-screen '
     >
-      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white '>
         <div>
-          <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>
+          <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline '>
             Experience
           </p>
           <p className='py-6'>These are the technologies I've worked with</p>
         </div>
 
-        <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-center py-8 px-4 sm:px-0'>
+        
+        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
@@ -42,7 +43,7 @@ const Experience = () => {
               <img
                 src={src}
                 alt={`${title} logo`}
-                className='w-16 sm:w-20 mx-auto'
+                className='w-20 mx-auto '
               />
               <p className='mt-4'>{title}</p>
             </div>
